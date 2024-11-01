d374c462c2101f06f7db6c4aa9cec511ec1d10bc
Sagar Dhadke <peaceunique2003@gmail.com>
2024-11-01 17:11:18 +0530

    Added No Internet

--- lib/Exports/myExports.dart
+++ lib/Exports/myExports.dart
@@ -3,6 +3,9 @@ export 'package:flutter/material.dart';
 //Env
 export 'package:flutter_dotenv/flutter_dotenv.dart';
 
+//No Internet 
+export 'package:rto/Utils/Helper/Connections/no_internet.dart';
+
 //Screens
 export 'package:rto/dashboard.dart';
 
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:rto/Utils/Helper/Connections/no_internet.dart';
 import 'Exports/myExports.dart';
 
 class MyDashboard extends StatefulWidget {
@@ -11,6 +15,9 @@ class MyDashboard extends StatefulWidget {
 class _MyDashboardState extends State<MyDashboard> {
   String baseUri = dotenv.env['BASE_URL'] ?? '';
   List<CategoryList>? ofCategoryList;
+  bool isConnectedToInternet = false;
+
+  StreamSubscription? _internetConnectionStreamSubscription;
 
   void getCategoryList() async {
     try {
@@ -33,6 +40,34 @@ class _MyDashboardState extends State<MyDashboard> {
   void initState() {
     super.initState();
     getCategoryList();
+
+    //Internet Checker
+     _internetConnectionStreamSubscription =
+        InternetConnection().onStatusChange.listen((event) {
+      switch (event) {
+        case InternetStatus.connected:
+          setState(() {
+            isConnectedToInternet = true;
+          });
+          break;
+        case InternetStatus.disconnected:
+          setState(() {
+            isConnectedToInternet = false;
+          });
+          break;
+        default:
+          setState(() {
+            isConnectedToInternet = false;
+          });
+          break;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+     _internetConnectionStreamSubscription?.cancel();
+    super.dispose();
   }
 
   @override
@@ -42,16 +77,14 @@ class _MyDashboardState extends State<MyDashboard> {
         title: Text("My Dashboard"),
         backgroundColor: MyColors.appBar,
       ),
-      body: Column(
+      body: isConnectedToInternet ? Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            "${baseUri}/category_list",
-            style: TextStyle(color: MyColors.textPrimary),
-          )
+          
+         
         ],
-      ),
+      ) : NoInternet()
     );
   }
 }
