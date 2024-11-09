5b69defb212aea453a66dce3a1c18ac47f305c63
Sagar Dhadke <peaceunique2003@gmail.com>
2024-11-09 12:56:03 +0530

    Added Traffic Rules Screen

---
+++ lib/Views/Driving_Instrucation.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyDriving_Instruction extends StatelessWidget {
+  const MyDriving_Instruction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
