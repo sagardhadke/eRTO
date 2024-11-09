bc203d2e74234b76039b5feffdebb16c05bdec8d
Sagar Dhadke <peaceunique2003@gmail.com>
2024-11-09 12:57:12 +0530

    Added Driving Instrction Screen

---
+++ lib/Views/TrafficRules.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyTrafficRules extends StatelessWidget {
+  const MyTrafficRules({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
