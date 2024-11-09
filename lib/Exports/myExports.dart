a781e09e641fa600bb1eb60a6b7c7022b7287fd8
Sagar Dhadke <peaceunique2003@gmail.com>
2024-11-09 12:57:49 +0530

    Update the Export File

--- lib/Exports/myExports.dart
+++ lib/Exports/myExports.dart
@@ -3,7 +3,7 @@ export 'package:flutter/material.dart';
 //Env
 export 'package:flutter_dotenv/flutter_dotenv.dart';
 
-//No Internet 
+//No Internet
 export 'package:rto/Utils/Helper/Connections/no_internet.dart';
 
 export 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
@@ -18,14 +18,13 @@ export 'package:rto/Views/Category.dart';
 export 'package:rto/Views/Terms.dart';
 export 'package:rto/Views/Instruction.dart';
 export 'package:rto/Views/Exam.dart';
-
-
+export 'package:rto/Views/TrafficRules.dart';
+export 'package:rto/Views/Driving_Instrucation.dart';
 
 //Models
 export 'package:rto/Model/categoryList.dart';
 export 'package:rto/Model/testCategory.dart';
 export 'package:rto/Model/examModel.dart';
-
 
 //Controller
 export 'package:rto/Controller/category_controller.dart';
@@ -42,10 +41,8 @@ export 'package:rto/Widgets/uiHelper.dart';
 //Text Helper
 export 'package:rto/Widgets/textHelper.dart';
 
-
 //Google Fonts
 export 'package:google_fonts/google_fonts.dart';
-
 
 //Logger
 export 'package:logger/logger.dart';
@@ -53,14 +50,11 @@ export 'package:logger/logger.dart';
 //Cached Image
 export 'package:cached_network_image/cached_network_image.dart';
 
-//ReadMore 
+//ReadMore
 export 'package:readmore/readmore.dart';
-
 
 //Colors
 export 'package:rto/Utils/constants/colors.dart';
 
 export 'dart:async';
 export 'dart:convert';
-
-
