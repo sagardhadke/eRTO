fce9c06cc225076b4dc1ab8dbeebab0b0eed28c9
Sagar Dhadke <peaceunique2003@gmail.com>
2024-11-12 15:51:27 +0530

    Added onBoard Logic

--- lib/Views/Splash.dart
+++ lib/Views/Splash.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rto/Exports/myExports.dart';
 import 'package:rto/Views/Category.dart';
-
+import 'package:rto/Views/onBoard/onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MySplashScreen extends StatefulWidget {
   const MySplashScreen({super.key});
@@ -10,12 +12,31 @@ class MySplashScreen extends StatefulWidget {
 }
 
 class _MySplashScreenState extends State<MySplashScreen> {
+  void checkUseronBoard() async {
+    try {
+      final SharedPreferences myprefs = await SharedPreferences.getInstance();
+      bool? onBoard = myprefs.getBool("onBoard");
+
+      if (onBoard == null || !onBoard) {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => MyOnBoarding()));
+      } else {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => MyDashboard()));
+      }
+    } catch (e) {
+      Uihelper.logger.e("${e.toString()}");
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyOnBoarding()));
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(microseconds: 1500), () {
+    Future.delayed(Duration(seconds: 2), () {
       return Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => MyCategory()));
+          context, MaterialPageRoute(builder: (context) => MyDashboard()));
     });
   }
 
